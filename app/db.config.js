f5d467943a3a23f9cc6895ba62835c67a0f5176d
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2023-02-12 21:31:19 +0700

    edit: set env in Docker

--- app/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: process.env.HOST || "localhost",
-    USER: process.env.USER || "root",
-    PASSWORD: process.env.PASSWORD || "",
-    DB: process.env.DB || "comcamp",
-}
